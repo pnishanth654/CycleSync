339437134c9d0711eae81f963ae95ae48471349e
Nishanth P <pnishanth654@gmail.com>
2026-03-24 12:40:10 +0530

    Update db path for Render production disk

--- ViteApp/server/database.js
+++ ViteApp/server/database.js
@@ -1,7 +1,7 @@
 const sqlite3 = require('sqlite3').verbose();
 const path = require('path');
 
-const dbPath = path.resolve(__dirname, 'cyclesync.db');
+const dbPath = process.env.NODE_ENV === 'production' ? '/data/cyclesync.db' : path.resolve(__dirname, 'cyclesync.db');
 const db = new sqlite3.Database(dbPath, (err) => {
   if (err) console.error('DB connect err', err);
   else console.log('Connected to CycleSync SQLite database.');
